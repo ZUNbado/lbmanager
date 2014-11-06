9c45e04a383f27e7692f6977d163c3ad7f69f770
ZUNbado <git@acs.li>
2014-11-06 17:28:26 +0100

    changing nginx model template and configuration

--- apps/nginx/admin.py
+++ apps/nginx/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import HostConfig, VirtualHostTemplate, NginxVirtualHost, AuthUser, Location
+from .models import HostConfig, NginxVirtualHost, AuthUser, Location
 
 
 admin.site.register(HostConfig)
-admin.site.register(VirtualHostTemplate)
 admin.site.register(NginxVirtualHost)
 admin.site.register(AuthUser)
 admin.site.register(Location)
--- apps/nginx/models.py
+++ apps/nginx/models.py
@@ -13,36 +13,32 @@ class NginxDefaults(models.Model):
 # Clase per opcions de nginx, encara per definir
 class HostConfig(NginxDefaults):
     group = models.ForeignKey(Group)
-    
-
-class VirtualHostTemplate(NginxDefaults):
-    TYPES = (
-        ('proxy', 'Proxy'),
-        ('files', 'Static'),
-    )
-    name = models.CharField(max_length=200)
-    type = models.CharField(max_length=5,choices=TYPES,default='proxy')
-
-class NginxVirtualHost(NginxDefaults):
-    name = models.CharField(max_length=200)
-    template = models.ForeignKey(VirtualHostTemplate)
-    cluster = models.ManyToManyField(Cluster)
-    extraconf = models.TextField(null=True,blank=True)
-    access_log = models.CharField(max_length=200,null=True,blank=True)
-    ssl_cert = models.TextField(null=True,blank=True)
-    ssl_key = models.TextField(null=True,blank=True)
-    ssl_ca = models.TextField(null=True,blank=True)
-
 
 class AuthUser(NginxDefaults):
     name = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
 
 class Location(NginxDefaults):
+    TYPES = (
+        ('proxy', 'Proxy'),
+        ('files', 'Static'),
+    )
     name = models.CharField(max_length=200)
-    path = models.CharField(max_length=200)
+    path_url = models.CharField(max_length=200)
+    backend_type = models.CharField(max_length=5,choices=TYPES,default='proxy')
+    path_fs = models.CharField(max_length=200,null=True,blank=True)
     auth_basic_enabled = models.BooleanField(default=False)
     auth_basic_msg = models.CharField(max_length=200,null=True,blank=True)
     users = models.ManyToManyField(AuthUser,null=True,blank=True)
     ip_allow_enabled = models.BooleanField(default=False)
     ip_allow_list = models.CharField(max_length=200,null=True,blank=True)
+
+class NginxVirtualHost(NginxDefaults):
+    name = models.CharField(max_length=200)
+    cluster = models.ManyToManyField(Cluster)
+    location = models.ManyToManyField(Location)
+    extraconf = models.TextField(null=True,blank=True)
+    access_log = models.CharField(max_length=200,null=True,blank=True)
+    ssl_cert = models.TextField(null=True,blank=True)
+    ssl_key = models.TextField(null=True,blank=True)
+    ssl_ca = models.TextField(null=True,blank=True)
