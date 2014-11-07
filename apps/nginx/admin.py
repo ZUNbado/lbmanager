15be8d101da25bd613e4789d827651d7b33ba0b6
ZUNbado <git@acs.li>
2014-11-07 13:21:27 +0100

    testing admin

--- apps/nginx/admin.py
+++ apps/nginx/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
 from .models import HostConfig, NginxVirtualHost, AuthUser, Location
 
+class LocationInline(admin.StackedInline):
+    model = NginxVirtualHost.location.through
+    extra = 1
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'path_url', 'enabled')
+        }),
+    )
+    
+
 class NginxVirtualHostAdmin(admin.ModelAdmin):
     model = NginxVirtualHost
+    inlines = [LocationInline, ]
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'cluster', 'enabled'),
+        }),
+        ('Extra', {
+            'classes': ('collapse',),
+            'fields': ('access_log', 'extraconf', 'redirect_type'),
+        }),
+        ('SSL', {
+            'classes': ('collapse',),
+            'fields': ('ssl_cert', 'ssl_key', 'ssl_ca'),
+        })
+    )
 
 admin.site.register(HostConfig)
 admin.site.register(NginxVirtualHost, NginxVirtualHostAdmin)
