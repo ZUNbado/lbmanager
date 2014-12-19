68458a39e735abfffeff9fc41b62b83cd36e641d
ZUNbado <git@acs.li>
2014-12-19 22:32:50 +0100

    fixing templates

--- apps/status/forms.py
+++ apps/status/forms.py
@@ -10,8 +10,8 @@ def past():
     return datetime.today() - timedelta(hours=1)
 
 class GraphForm(forms.Form):
-    start_date = forms.DateField(label='Data',widget=SelectDateWidget(),initial=past())
-    start_time = forms.TimeField(label='Data inici',widget=SelectTimeWidget(),initial=past())
+    start_date = forms.DateField(label='Data Inici',widget=SelectDateWidget(),initial=past())
+    start_time = forms.TimeField(label='Hora inici',widget=SelectTimeWidget(),initial=past())
 
-    end_date = forms.DateField(label='Data',widget=SelectDateWidget(),initial=now())
-    end_time = forms.TimeField(label='Data inici',widget=SelectTimeWidget(),initial=now())
+    end_date = forms.DateField(label='Data fi',widget=SelectDateWidget(),initial=now())
+    end_time = forms.TimeField(label='Hora fi',widget=SelectTimeWidget(),initial=now())
